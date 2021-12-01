950cccc1e7c6533592410b58893653cb16dbf4bf
Romsgs <rsalobrena@gmail.com>
2021-11-30 21:29:49 -0300

    view de contatos pronta com modificacao de variaveis

--- scriptPraEnviarOEmail.js
+++ scriptPraEnviarOEmail.js
@@ -1,12 +1,13 @@
 function enviar(params){
     var tempParams = {
         from_name: document.getElementById('fromName').value,
-        to_name: document.getElementById('toName').value,
+        email: document.getElementById('emailDoUsuario').value,
         message: document.getElementById('msg').value
     };
     // deve ser passados 3 parametros, ID do servico de email,
     // segundo, o ID do template, terceiro, um obj com as variaveis a serem inseridas no template
     emailjs.send('service_qgmf1cp', 'template_4e44swz', tempParams).then(res =>{
         console.log('enviado', res.status)
+        alert('Menssagem enviada com sucesso!')
     })
 }

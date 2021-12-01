56c74156e44cd9e1ed38998fe657a42b1e16af7e
Romsgs <rsalobrena@gmail.com>
2021-11-30 21:21:53 -0300

    inserido o reCaptcha

--- scriptPraEnviarOEmail.js
+++ scriptPraEnviarOEmail.js
@@ -1,7 +1,7 @@
 function enviar(params){
     var tempParams = {
         from_name: document.getElementById('fromName').value,
-        email: document.getElementById('emailDoUsuario').value,
+        email: document.getElementById('email').value,
         message: document.getElementById('msg').value
     };
     // deve ser passados 3 parametros, ID do servico de email,
@@ -9,8 +9,13 @@ function enviar(params){
     emailjs.send('service_qgmf1cp', 'template_4e44swz', tempParams).then(res =>{
         console.log('enviado', res.status)
         alert('Menssagem enviada com sucesso!')
-    })
-}
+        from_name.value = ''
+        email.value = ''
+        message.value = ''
 
+
+    })
+
+}
 // chave secreta do recaptcha 6LfPvGwdAAAAAFGFdCmCcXua_LawM_8-2_LwgD_d
 // chave do site 6LfPvGwdAAAAAAdVMMcyovxjV1W1OhWpiEAtAxiX
